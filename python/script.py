8da4a689bc0c200f0848ccbd6167ed623ee63ca4
atrusov <atrusov@intech-global.com>
2021-10-01 21:28:53 +0300

    test

--- python/script.py
+++ python/script.py
@@ -3,28 +3,21 @@ import psycopg2
 
 @route('/script')
 def index():
-   #Establishing the connection
-   conn = psycopg2.connect(
-              user='postgres', password='123456Pp', host='127.0.0.1', port= '5432'
-   )
-   #Creating a cursor object using the cursor() method
-   cursor = conn.cursor()
-
-   #Doping EMPLOYEE table if already exists.
-   cursor.execute("DROP TABLE IF EXISTS TEST")
-
-   #Creating table as per requirement
-   sql ='''CREATE TABLE TEST(
-      FIRST_NAME CHAR(20) NOT NULL,
-      LAST_NAME CHAR(20),
-      AGE INT,
-      SEX CHAR(1),
-      INCOME FLOAT
-      )'''
-   cursor.execute(sql)
-   conn.commit()
-   #Closing the connection
-   conn.close()
+   # conn = psycopg2.connect(
+   #            user='postgres', password='123456Pp', host='127.0.0.1', port= '5432'
+   # )
+   # cursor = conn.cursor()
+   # cursor.execute("DROP TABLE IF EXISTS TEST")
+   # sql ='''CREATE TABLE TEST(
+   #    FIRST_NAME CHAR(20) NOT NULL,
+   #    LAST_NAME CHAR(20),
+   #    AGE INT,
+   #    SEX CHAR(1),
+   #    INCOME FLOAT
+   #    )'''
+   # cursor.execute(sql)
+   # conn.commit()
+   # conn.close()
    return "Table created successfull!"
 
 run(host='localhost', port=8080)
