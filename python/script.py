34f2cc8fd4e66967b670f4e27d4b1d5aa2e5e63d
atrusov <atrusov@intech-global.com>
2021-10-03 18:08:30 +0300

    fix

--- python/script.py
+++ python/script.py
@@ -3,21 +3,21 @@ import psycopg2
 
 @route('/script')
 def index():
-   conn = psycopg2.connect(
-              user='postgres', password='123456Pp', host='127.0.0.1', port= '5432'
-   )
-   cursor = conn.cursor()
-   cursor.execute("DROP TABLE IF EXISTS TEST")
-   sql ='''CREATE TABLE TEST(
-      FIRST_NAME CHAR(20) NOT NULL,
-      LAST_NAME CHAR(20),
-      AGE INT,
-      SEX CHAR(1),
-      INCOME FLOAT
-      )'''
-   cursor.execute(sql)
-   conn.commit()
-   conn.close()
+   # conn = psycopg2.connect(
+   #            user='postgres', password='123456Pp', host='127.0.0.1', port= '5432'
+   # )
+   # cursor = conn.cursor()
+   # cursor.execute("DROP TABLE IF EXISTS TEST")
+   # sql ='''CREATE TABLE TEST(
+   #    FIRST_NAME CHAR(20) NOT NULL,
+   #    LAST_NAME CHAR(20),
+   #    AGE INT,
+   #    SEX CHAR(1),
+   #    INCOME FLOAT
+   #    )'''
+   # cursor.execute(sql)
+   # conn.commit()
+   # conn.close()
    return "Table created successfull!"
 
-run(host='localhost', port=8080)
+run(host='0.0.0.0', port=8080)
