ad84482fb718fb50a43fd1bf0a9000c719f5e93e
atrusov <atrusov@intech-global.com>
2021-10-04 11:57:36 +0300

    fix script

--- python/script.py
+++ python/script.py
@@ -1,23 +1,29 @@
-from bottle import route, run, template
+from bottle import route, run
 import psycopg2
 
 @route('/script')
 def index():
-   # conn = psycopg2.connect(
-   #            user='postgres', password='123456Pp', host='127.0.0.1', port= '5432'
-   # )
-   # cursor = conn.cursor()
-   # cursor.execute("DROP TABLE IF EXISTS TEST")
-   # sql ='''CREATE TABLE TEST(
-   #    FIRST_NAME CHAR(20) NOT NULL,
-   #    LAST_NAME CHAR(20),
-   #    AGE INT,
-   #    SEX CHAR(1),
-   #    INCOME FLOAT
-   #    )'''
-   # cursor.execute(sql)
-   # conn.commit()
-   # conn.close()
-   return "Table created successfull!"
+    conn = None
+    try:
+        conn = psycopg2.connect(
+                  user='postgres', password='123456Pp', host='postgres', port='5432'
+        )
+        cursor = conn.cursor()
+        cursor.execute("DROP TABLE IF EXISTS TEST")
+        sql ='''CREATE TABLE TEST(
+          FIRST_NAME CHAR(20) NOT NULL,
+          LAST_NAME CHAR(20),
+          AGE INT,
+          SEX CHAR(1),
+          INCOME FLOAT
+          )'''
+        cursor.execute(sql)
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        return error
+    finally:
+        if conn is not None:
+            conn.close()
+    return "Table created successfully!\n"
 
 run(host='0.0.0.0', port=8080)
